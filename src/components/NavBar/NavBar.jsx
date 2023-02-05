263eab0ebc61dbf93a71da59089a4efffe12fa2f
CharlieMickey <charles.f.mickey@gmail.com>
2023-02-05 16:49:24 -0600

    add nav link to home page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,6 +6,7 @@ const NavBar = ({ user, handleLogout }) => {
       {user ?
         <ul>
           <li>Welcome, {user.name}</li>
+          <li><Link to="/">Home</Link></li>
           <li><Link to="/profiles">Profiles</Link></li>
           <li><Link to="" onClick={handleLogout}>LOG OUT</Link></li>
           <li><Link to="/change-password">Change Password</Link></li>
