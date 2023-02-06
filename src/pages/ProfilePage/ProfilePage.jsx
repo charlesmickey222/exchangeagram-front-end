bb7604cdc51489b728dc27ed81d09abe0e52050e
CharlieMickey <charles.f.mickey@gmail.com>
2023-02-06 14:34:07 -0600

    add profilepicture and username and post count to profile page

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,5 +1,6 @@
 import { useLocation  } from "react-router-dom";
 import { useState,useEffect } from "react";
+import './ProfilePage.css'
 const ProfilePage = () => {
   const location = useLocation()
   const [targetProfile,setTargetProfile] = useState({})
@@ -9,7 +10,22 @@ const ProfilePage = () => {
   
   return ( 
   <>
-    <h1>{targetProfile.name}</h1>
+    <div className="profileHeader">
+      <div className="profPic">
+      <img src={`${targetProfile.photo}`} alt='profile'/>
+      </div>
+      <p className="username">{targetProfile.name}</p>
+    </div>
+    <div className="statsContainer">
+      <table>
+        <tr>
+          <th>posts</th>
+        </tr>
+        <tr>
+          <td>{targetProfile.posts.length}</td>
+        </tr>
+      </table>
+    </div>
   </> );
 }
 
