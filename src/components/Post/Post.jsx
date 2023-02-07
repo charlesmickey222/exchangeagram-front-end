9c1cbbb9a62ba3d928a851e0f77505ec2e8621bd
Drew Neece <dneece5@aggienetwork.com>
2023-02-06 21:38:56 -0600

    add delete post functionality

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -12,6 +12,7 @@ const Post = (props) => {
     <img src={props.post.photo} alt="user's post here" />
     <p>{props.post.createdAt}</p>
     <p>Caption: {props.post.caption}</p>
+    <button onClick={() => props.handleDeletePost(props.post.id)}>Delete Post</button>
     <h5>Comments:</h5>
     {props.post.comments?.map(comment =>
       <Comment key={comment._id} comment={comment} />
