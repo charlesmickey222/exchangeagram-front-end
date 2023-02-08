dbe607eb673c56c978917cdb990b344777046991
MannySanz <mannysanz715@gmail.com>
2023-02-08 11:17:53 -0800

    Add messages state

--- src/components/CreateMesageForm/CreateMessageForm.jsx
+++ src/components/CreateMesageForm/CreateMessageForm.jsx
@@ -2,13 +2,19 @@ import { useState } from "react"
 
 const NewMessage = (props) => {
   const [form, setForm] = useState({ text: '' })
+  const [messages, setMessages] = useState([])
   const handleChange = ({ target }) => {
     setForm({ ...form, [target.name]: target.value})
   }
   const handleSubmit = (e) => {
     e.preventDefault()
-    props.handleAddMessage(props.profileId, form)
-    setForm({ text: '' })
+    handleAddMessage(props.user._id, form.text)
+  }
+  
+  const handleAddMessage = async (profileId, messageData) => {
+    console.log(messages)
+    setMessages({...messages, messageData})
+    setForm({text: ''})
   }
   return (
     <form onSubmit={handleSubmit}>
--- src/pages/Messages/Messages.jsx
+++ src/pages/Messages/Messages.jsx
@@ -5,7 +5,7 @@ const Messages = (props) => {
   <>
   <p>This is the message page</p>
   <p>Send Message</p>
-  <NewMessage handleAddMessage={props.handleAddMessage}/>
+  <NewMessage user={props.user} handleAddMessage={props.handleAddMessage}/>
 
   </>
   );
