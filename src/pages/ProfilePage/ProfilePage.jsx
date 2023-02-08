22496630fcba005eac4bdd1bfccdfdfc15eca4f1
CharlieMickey <charles.f.mickey@gmail.com>
2023-02-08 13:43:23 -0600

    message route fix

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,27 +1,30 @@
 import { useLocation  } from "react-router-dom";
 import { useState,useEffect } from "react";
 import './ProfilePage.css'
-/*import { Link } from "react-router-dom";*/
+import { Link } from "react-router-dom";
 import PostPreview from "../../components/Post/PostPreview";
 
 const ProfilePage = (props) => {
   const location = useLocation()
   const [targetProfile,setTargetProfile] = useState({})
   const [ownsProfile,setOwnsProfile] = useState({})
+  const [linkName, setLinkName]= useState({})
 
   useEffect(() => {
     console.log(location.state.profile)
     setOwnsProfile(Boolean(props.userProfile._id === location.state.profile._id))
     if (ownsProfile){
       setTargetProfile(props.userProfile)
+      setLinkName(props.userProfile.name.replaceAll(' ','_'))
     }else{
     setTargetProfile(location.state.profile)
     }
+    
   }, [location.state.profile, props.user, props.userProfile, ownsProfile])
 
-
+  
   if (!targetProfile) return <p>loading</p>
-
+  
   return ( 
   <>
     <section>
@@ -32,6 +35,7 @@ const ProfilePage = (props) => {
       <p className="username">{targetProfile.name}</p>
       <div className="statsContainer">
         posts: {targetProfile.posts?targetProfile.posts.length:0}
+        <Link to={`messages`}>messages</Link>
       </div>
     </div>
     {ownsProfile ?  
