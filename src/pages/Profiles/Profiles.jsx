e38691c8b5687a85fdd3347cb1cc998ff89be983
mannysanz715 <mannysanz715@gmail.com>
2023-02-10 09:27:39 -0800

    Profiles

--- src/pages/Profiles/Profiles.jsx
+++ src/pages/Profiles/Profiles.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import * as profileService from '../../services/profileService'
 import {Link} from 'react-router-dom'
 import ProfileIcon from '../../assets/icons/profile.png'
+import './Profiles.css'
 
 const Profiles = () => {
   const [profiles, setProfiles] = useState([])
@@ -16,21 +17,24 @@ const Profiles = () => {
 
   return (
     <>
+
       <h1>Profiles</h1>
       {profiles.length ? 
         <>
+    <div className="profile-container">
           <ul>
             {profiles.map(profile =>
               <li key={`${profile._id}`}>
                 <img src={`${profile.photo ? profile.photo : ProfileIcon}`} style={{height:'10vh'}} alt='profile'/>
-                <Link to={`${profile.name.replaceAll(' ','_')}`} state={{profile}}>{profile.name}</Link>
+                <Link to={`${profile.name.replaceAll(' ','_')}`} className="link"state={{profile}}>{profile.name}</Link>
               </li>
             )}
           </ul>
+          </div>
         </>
       :
-        <p>No Profiles Yet</p>
-      }
+      <p>No Profiles Yet</p>
+    }
     </>
   )
 }
