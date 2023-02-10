61ab47a9655aedcd558d898432fedc52d74f9d00
Drew Neece <dneece5@aggienetwork.com>
2023-02-09 21:25:15 -0600

    fix indentations, function declarations, console.logs, etc

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,8 +1,8 @@
-import { useLocation  } from "react-router-dom";
-import { useState,useEffect } from "react";
+import { useLocation  } from "react-router-dom"
+import { useState,useEffect } from "react"
 import './ProfilePage.css'
-import { Link } from "react-router-dom";
-import PostPreview from "../../components/Post/PostPreview";
+import { Link } from "react-router-dom"
+import PostPreview from "../../components/Post/PostPreview"
 import ProfileIcon from '../../assets/icons/profile.png'
 
 const ProfilePage = (props) => {
@@ -11,53 +11,54 @@ const ProfilePage = (props) => {
   const [ownsProfile,setOwnsProfile] = useState({})
 
   useEffect((evt) => {
-
     setOwnsProfile(Boolean(props.userProfile._id === location.state.profile._id))
-    if (ownsProfile){
+    if (ownsProfile) {
       setTargetProfile(props.userProfile)
-    }else{
-    setTargetProfile(location.state.profile)
+    } else {
+      setTargetProfile(location.state.profile)
     }
     localStorage.setItem('targetProfile', JSON.stringify(targetProfile))
   }, [location.state.profile, props.user, props.userProfile, ownsProfile, targetProfile])
 
-  
   if (!targetProfile) return <p>Loading...</p>
   
   return ( 
-  <main>
-    <section>
-    <div className="profileHeader">
-      <div className="profPic">
-      <img src={`${targetProfile.photo ? targetProfile.photo : ProfileIcon}`} alt='profile'/>
-      </div>
-      <p className="username">{targetProfile.name}</p>
-      <div className="statsContainer">
-        Posts: {targetProfile.posts?targetProfile.posts.length:0}
-        <Link to={`messages`}>Messages</Link>
-      </div>
-    </div>
-    {ownsProfile ?  
-        <div className="postContainer">
-        {targetProfile.posts?
-            targetProfile.posts.map(post =><PostPreview key={`${post._id}`} className='preview' post={post._id}/>)
-          :
-        'no posts here'}
-      </div>
-      :
-    <div className="postContainer">
-      {targetProfile.posts?
-          targetProfile.posts.map(post =>
-              <div key={`${post}`}>
-                <PostPreview className='preview' post={post}/>
-              </div>
+    <main>
+      <section>
+        <div className="profileHeader">
+          <div className="profPic">
+            <img src={`${targetProfile.photo ? targetProfile.photo : ProfileIcon}`} alt='profile'/>
+          </div>
+          <p className="username">{targetProfile.name}</p>
+          <div className="statsContainer">
+            Posts: {targetProfile.posts ? targetProfile.posts.length : 0}
+            <Link to={`messages`}>Messages</Link>
+          </div>
+        </div>
+        {ownsProfile ?  
+          <div className="postContainer">
+            {targetProfile.posts ?
+              targetProfile.posts.map(post =><PostPreview key={`${post._id}`} className='preview' post={post._id}/>)
+            :
+              <p>No Posts Yet</p>
+            }
+          </div>
+        :
+          <div className="postContainer">
+            {targetProfile.posts ?
+              targetProfile.posts.map(post =>
+                <div key={`${post}`}>
+                  <PostPreview className='preview' post={post}/>
+                </div>
               )
-              :
-      <>'no posts here'</>}
-    </div>
-    }
-    </section>
-  </main> )
+            :
+              <p>No Posts Yet</p>
+            }
+          </div>
+        }
+      </section>
+    </main>
+  )
 }
 
-export default ProfilePage;
+export default ProfilePage
